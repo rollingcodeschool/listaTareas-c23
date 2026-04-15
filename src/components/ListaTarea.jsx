7381abb81585fc38f5bac160e70b32db1d014462
Emilse Arias <emilsearias@gmail.com>
2026-04-15 09:31:54 -0300

    dejo un unico ItemTarea en el componente ListaTarea

--- src/components/ListaTarea.jsx
+++ src/components/ListaTarea.jsx
@@ -4,9 +4,6 @@ const ListaTarea = () => {
   return (
     <ul className="list-group">
       <ItemTarea></ItemTarea>
-      <ItemTarea></ItemTarea>
-      <ItemTarea></ItemTarea>
-      <ItemTarea></ItemTarea>
     </ul>
   );
 };
